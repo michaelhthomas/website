6717bbe8d032b808c5aaae178fdcece1f28f7db3
Michael Thomas <michaelhthomas@outlook.com>
2021-07-09 09:27:05 -0400

    Update author tagline

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -1,19 +1,19 @@
 <template>
 	<div class="author">
 
-		<g-image alt="Author image" class="author__image" src="~/assets/images/author.jpg" width="180" height="180" blur="5" />
+		<g-image alt="Author image" class="author__image" src="~/assets/images/author.png" width="180" height="180" blur="5" />
 
 		<h1 v-if="showTitle" class="author__site-title">
 			{{ $static.metadata.siteName }}
 		</h1>
 
 		<p class="author__intro">
-			A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.
+			A blog on Linux, NixOS, web development, home automation, and more.
 		</p>
 
 		<p class="author__links">
-			<a href="//twitter.com/gridsome">Follow on Twitter</a>
-			<a href="//github.com/gridsome/gridsome-starter-blog">GitHub</a>
+			<a href="//reddit.com/u/linux_overuser">Reddit</a>
+			<a href="//github.com/michaelhthomas">GitHub</a>
 		</p>
 
 	</div>
