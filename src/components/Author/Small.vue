dbd229c864a75b9fdf584188b891ce58dbeaebb2
Michael Thomas <michaelhthomas@outlook.com>
2022-01-16 06:06:16 +0000

    🤵 Update Author blurb card

--- src/components/Author/Small.vue
+++ src/components/Author/Small.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="rounded-md bg-true-gray-200 dark:bg-true-gray-800 p-10 flex gap-3 flex-wrap sm:flex-nowrap justify-center text-center sm:text-left">
+  <div class="rounded-md bg-light-700 dark:bg-true-gray-800 p-10 flex gap-3 flex-wrap sm:flex-nowrap justify-center text-center sm:text-left">
     <div class="flex-shrink-0 rounded-full h-40 mb-4 w-40 md:mb-0">
       <g-image 
         alt="avatar"
@@ -8,14 +8,14 @@
       />
     </div>
 
-    <div class="flex-grow flex flex-col justify-center">
-      <h1 class="text-3xl font-semibold text-true-gray-900 dark:text-true-gray-100 mb-2">
+    <div class="flex-grow flex flex-col justify-center text-true-gray-800 dark:text-true-gray-100">
+      <h1 class="text-3xl font-semibold mb-2">
         Michael Thomas
       </h1>
       <p>
         I'm Michael Thomas, a web developer and systems engineer from Columbia, South Carolina.
         I love to build stuff, break stuff, and most importantly, learn from the experience. I 
-        like to blog about my experiences with Linux, selfhosting, and app development.
+        like to blog about my experiences with Linux, self-hosting, and app development.
       </p>
     </div>
   </div>
