36ad379d566a001b6b07aa9b374661c2ab347f78
Michael Thomas <michaelhthomas@outlook.com>
2022-06-25 19:53:43 -0400

    🎉 Update Partytown config

--- gridsome.config.js
+++ gridsome.config.js
@@ -52,7 +52,8 @@ module.exports = {
     {
       use: 'gridsome-plugin-partytown',
       options: {
-        debug: true,
+        debug: false,
+        lib: "partytown-lib",
         forward: ["dataLayer.push"]
       }
     }
