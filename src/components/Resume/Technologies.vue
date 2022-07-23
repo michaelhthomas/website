90bf6555469b9be9d93acdeeff292a1de91286f5
Michael Thomas <michaelhthomas@outlook.com>
2022-07-23 18:11:48 -0400

    Update technologies list

--- src/components/Resume/Technologies.vue
+++ src/components/Resume/Technologies.vue
@@ -3,19 +3,19 @@ import ResumeTechnologyCard from '~/components/Resume/TechnologyCard.vue'
 
 const technologies = [
   {
-    name: "JavaScript",
-    link: "https://en.wikipedia.org/wiki/JavaScript",
-    icon: "custom:javascript"
+    name: "Azure",
+    link: "https://azure.microsoft.com",
+    icon: "logos:microsoft-azure"
   },
   {
-    name: "HTML5",
-    link: "https://en.wikipedia.org/wiki/HTML",
-    icon: "vscode-icons:file-type-html"
+    name: "Azure Pipelines",
+    link: "https://azure.microsoft.com/en-us/services/devops/pipelines/",
+    icon: "vscode-icons:file-type-azurepipelines"
   },
   {
-    name: "Nuxt.js",
-    link: "https://nuxtjs.org",
-    icon: "logos:nuxt-icon"
+    name: "C#",
+    link: "https://docs.microsoft.com/en-us/dotnet/csharp/",
+    icon: "vscode-icons:file-type-csharp2"
   },
   {
     name: "Vue.js",
@@ -23,19 +23,35 @@ const technologies = [
     icon: "logos:vue"
   },
   {
-    name: "Windi CSS",
-    link: "https://windicss.org",
-    icon: "custom:windicss"
+    name: "Nuxt.js",
+    link: "https://nuxtjs.org",
+    icon: "logos:nuxt-icon"
   },
   {
-    name: "Node.js",
-    link: "https://nodejs.org",
-    icon: "logos:nodejs-icon"
+    name: "React",
+    link: "https://reactjs.org",
+    icon: "logos:react"
   },
+  {
+    name: "JavaScript",
+    link: "https://en.wikipedia.org/wiki/JavaScript",
+    icon: "custom:javascript"
+  },
+
   {
     name: "TypeScript",
     link: "https://typescriptlang.org",
     icon: "logos:typescript-icon"
+  },
+  {
+    name: "HTML5",
+    link: "https://en.wikipedia.org/wiki/HTML",
+    icon: "vscode-icons:file-type-html"
+  },
+  {
+    name: "Windi CSS",
+    link: "https://windicss.org",
+    icon: "custom:windicss"
   },
   {
     name: "Sass",
@@ -43,15 +59,9 @@ const technologies = [
     icon: "logos:sass"
   },
   {
-    name: "Figma",
-    link: "https://figma.com",
-    icon: "logos:figma"
-  },
-  {
-    name: "WordPress",
-    link: "https://wordpress.org",
-    icon: "mdi:wordpress",
-    color: "#3B82F6"
+    name: "Node.js",
+    link: "https://nodejs.org",
+    icon: "logos:nodejs-icon"
   },
   {
     name: "PHP",
@@ -64,11 +74,17 @@ const technologies = [
     link: "https://twig.symfony.com",
     icon: "vscode-icons:file-type-twig"
   },
+  {
+    name: "WordPress",
+    link: "https://wordpress.org",
+    icon: "mdi:wordpress",
+    color: "#3B82F6"
+  },
 ]
 </script>
 
 <template>
-  <div class="grid grid-cols-2 gap-1 mt-4 sm:grid-cols-3 md:grid-cols-4">
+  <div class="grid grid-cols-2 gap-1 mt-4 sm:grid-cols-3 md:grid-cols-5">
     <ResumeTechnologyCard
       v-for="(technology, index) in technologies"
       :key="`skill-${index}`"
