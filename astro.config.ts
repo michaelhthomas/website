bf782d16980ddbbb3729b5c79374c0a99d9c7014
Michael Thomas <michaelhthomas@outlook.com>
2024-04-09 23:55:34 -0400

    feat: support code titles and callouts

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
+
+// integrations
 import vue from '@astrojs/vue';
 import unocss from 'unocss/astro';
-import unpluginIcons from 'unplugin-icons/vite';
 import icon from 'astro-icon';
 import mdx from '@astrojs/mdx';
+
+// vite plugins
+import unpluginIcons from 'unplugin-icons/vite';
+
+// markdown
+import remarkCodeTitles from 'remark-code-title';
+import remarkDirective from 'remark-directive';
+import remarkCalloutDirectives from '@microflash/remark-callout-directives';
 
 // https://astro.build/config
 export default defineConfig({
@@ -21,5 +30,11 @@ export default defineConfig({
         compiler: 'vue3'
       })
     ]
+  },
+  markdown: {
+    shikiConfig: {
+      theme: 'material-theme-darker'
+    },
+    remarkPlugins: [remarkCodeTitles, remarkDirective, remarkCalloutDirectives]
   }
 });
